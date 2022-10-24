d9c9ed55e09ca0e7ef95bb91fb2bf373ccd77358
jessicakohh <juko95@me.com>
2022-10-24 15:08:30 +0900

    UI _ CreditCardCell

---
+++ CreditCardList/Controllers/CardListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CardListViewController.swift
+//  CreditCardList
+//
+//  Created by juyeong koh on 2022/10/24.
+//
+
+import UIKit
+
+class CardListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+}
---
+++ CreditCardList/Models/CreditCard.swift
@@ -0,0 +1,31 @@
+//
+//  CreditCard.swift
+//  CreditCardList
+//
+//  Created by juyeong koh on 2022/10/24.
+//
+
+import Foundation
+
+struct CreditCard: Codable {
+    
+    let id: Int
+    let rank: Int
+    let name: String
+    let cardImageURL: String
+    let promotionDetail: PromotionDetail
+    // 추후 사용자가 카드를 선택했을 때 사용
+    let isSelected: Bool?
+    
+}
+
+struct PromotionDetail: Codable {
+    let companyName: String
+    let period: String
+    let amount: Int
+    let condition: String
+    let benefitCondition: String
+    let benefitDerail: String
+    let benefitDate: String
+    
+}
