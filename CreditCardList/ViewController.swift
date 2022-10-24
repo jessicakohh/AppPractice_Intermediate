300d761c7d6f8816b11e688ad4195efaa3f6f40b
jessicakohh <juko95@me.com>
2022-10-24 14:00:55 +0900

    Initial Commit

---
+++ CreditCardList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreditCardList
+//
+//  Created by juyeong koh on 2022/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
